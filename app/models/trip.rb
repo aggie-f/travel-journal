58aba40b8f9f4738dae9af93fc1715a859e112fb
karolina langner <karolinalangner1@gmail.com>
2021-01-26 14:31:55 +0000

    removed photos from trip model so there is no error with seeds

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :owner, class_name: "User"
-  has_many :notes
-  has_many :goals
-  has_many_attached :photos
+  has_many :notes, dependent: :destroy
+  has_many :goals, dependent: :destroy
   has_many :collaborations
   has_many :guests, source: :user, through: :collaborations
 
