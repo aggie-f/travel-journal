a90e3b187b9ce836c1bac1f891a45c06472c1184
Agata Filus <agatafilus@gmail.com>
2021-01-26 17:42:48 +0000

    complete goals controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :trips do
     resources :notes, only:[:new, :create]
+    resources :goals, only:[:new, :create]
   end
 
   resources :notes, only: [:destroy, :update, :edit]
+  resources :goals, only: [:destroy, :update, :edit]
 end
